3a4b81e73e5abfa71f8f2d6399cfc91db58762cc
vietluu <74524082+vietluu@users.noreply.github.com>
2023-08-13 18:35:39 +0700

    fix vercel.json

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const upload = require('../utils/upload');
 const controller = require('../controllers/upload');
 
-router.post('/', upload.array('images'), async (req, res) => {
+router.post('/add', upload.array('images'), async (req, res) => {
   res.send(await controller.uploadimage(req));
 });
 module.exports = router;
